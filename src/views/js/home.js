80057944e4a8106e65c2c9d842f4f05f307ebd33
z3zzz <zabra7946@naver.com>
2022-05-05 12:57:25 +0900

    add: homepage image slider

--- src/views/js/home.js
+++ src/views/js/home.js
@@ -1,9 +1,17 @@
 // 이미지 슬라이드
-bulmaCarousel.attach('#slider', {
+const imageSlider = bulmaCarousel.attach('#slider', {
   //autoplay: true,
   autoplaySpeed: 5000,
   infinite: true,
   duration: 1200,
   pauseOnHover: false,
   navigation: false,
+});
+
+document.querySelector('.slider-arrow-left').addEventListener('click', () => {
+  imageSlider[0].previous();
+});
+
+document.querySelector('.slider-arrow-right').addEventListener('click', () => {
+  imageSlider[0].next();
 });
