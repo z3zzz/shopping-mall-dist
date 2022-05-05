fa64f1cfd7aa5faeb9949648fec627aed380d0d2
z3zzz <zabra7946@naver.com>
2022-05-06 07:51:25 +0900

    add cors middleware to prevent CORS Error

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
+import cors from 'cors';
 import express from 'express';
 import { viewsRouter, userRouter, categoryRouter } from './routers';
 import { errorLogger, errorHandler } from './middlewares';
 
 const app = express();
+
+// CORS 에러 방지
+app.use(cors());
 
 // POST 등의 요청과 함께 오는 json형태의 데이터를 인식하고 핸들링할 수 있게 함.
 app.use(express.json());
