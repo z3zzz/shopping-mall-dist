34a8edca5dd0ce418c5422d5ca1cacc11cd18161
z3zzz <zabra7946@naver.com>
2022-05-09 02:46:02 +0900

    add: products page

---
+++ src/views/js/products.js
@@ -0,0 +1,23 @@
+import { getImageUrl } from './common/aws-s3.js';
+import * as Api from './common/api.js';
+
+// 요소(element), input 혹은 상수
+const productItemContainer = document.querySelector('#productItemContainer');
+
+addAllElements()
+addAllEvents();
+
+
+// html에 요소를 추가하는 함수들을 묶어주어서 코드를 깔끔하게 하는 역할임.
+function addAllElements() {
+  addOptionsToSelectBox();
+}
+
+// addEventListener들을 묶어주어서 코드를 깔끔하게 하는 역할임.
+function addAllEvents() {}
+
+
+addProductItemsToContainer() {
+  const  
+
+}
