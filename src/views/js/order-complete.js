6ac6d06ef4666916940ae56d48844d036eb9c068
z3zzz <zabra7946@naver.com>
2022-05-16 04:08:51 +0900

    add: order-complete page

---
+++ src/views/js/order-complete.js
@@ -0,0 +1,20 @@
+import * as Api from './common/api.js';
+import { checkLogin, doLogout, navigate } from './common/useful-functions.js';
+
+// 요소(element), input 혹은 상수
+const logoutTag = document.querySelector('#logoutTag');
+const orderDetailButton = document.querySelector('#orderDetailButton');
+const shoppingButton = document.querySelector('#shoppingButton');
+
+checkLogin();
+addAllElements();
+addAllEvents();
+
+// html에 요소를 추가하는 함수들을 묶어주어서 코드를 깔끔하게 하는 역할임.
+function addAllElements() {}
+
+// addEventListener들을 묶어주어서 코드를 깔끔하게 하는 역할임.
+function addAllEvents() {
+  logoutTag.addEventListener('click', doLogout);
+  shoppingButton.addEventListener('click', navigate('/'));
+}
