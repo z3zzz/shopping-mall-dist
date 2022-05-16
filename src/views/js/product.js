3487211d9641dd5323eb299071307846f0a4bf2c
z3zzz <zabra7946@naver.com>
2022-05-16 21:23:04 +0900

    refactor: extracting db update code as function

--- src/views/js/product.js
+++ src/views/js/product.js
@@ -61,30 +61,7 @@ async function insertProductData() {
 
   addToCartButton.addEventListener('click', async () => {
     try {
-      // 장바구니 추가 시, indexedDB에 제품 데이터 및
-      // 주문수량 (기본값 1)을 저장함.
-      await addToDb('cart', { ...product, quantity: 1 }, id);
-
-      // 장바구니 요약(=전체 총합)을 업데이트함.
-      await putToDb('order', 'summary', (data) => {
-        // 기존 데이터를 가져옴
-        const count = data.productsCount;
-        const total = data.productsTotal;
-        const ids = data.ids;
-        const selectedIds = data.selectedIds;
-
-        // 기존 데이터가 있다면 1을 추가하고, 없다면 초기값 1을 줌
-        data.productsCount = count ? count + 1 : 1;
-
-        // 기존 데이터가 있다면 가격만큼 추가하고, 없다면 초기값으로 해당 가격을 줌
-        data.productsTotal = total ? total + price : price;
-
-        // 기존 데이터(배열)가 있다면 id만 추가하고, 없다면 배열 새로 만듦
-        data.ids = ids ? [...ids, id] : [id];
-
-        // 위와 마찬가지 방식
-        data.selectedIds = selectedIds ? [...selectedIds, id] : [id];
-      });
+      await insertDb(product);
 
       alert('장바구니에 추가되었습니다.');
     } catch (err) {
@@ -99,36 +76,45 @@ async function insertProductData() {
 
   purchaseButton.addEventListener('click', async () => {
     try {
-      // 장바구니 추가 시, indexedDB에 제품 데이터 및
-      // 주문수량 (기본값 1)을 저장함.
-      await addToDb('cart', { ...product, quantity: 1 }, id);
-
-      // 장바구니 요약(=전체 총합)을 업데이트함.
-      await putToDb('order', 'summary', (data) => {
-        // 기존 데이터를 가져옴
-        const count = data.productsCount;
-        const total = data.productsTotal;
-        const ids = data.ids;
-        const selectedIds = data.selectedIds;
-
-        // 기존 데이터가 있다면 1을 추가하고, 없다면 초기값 1을 줌
-        data.productsCount = count ? count + 1 : 1;
-
-        // 기존 데이터가 있다면 가격만큼 추가하고, 없다면 초기값으로 해당 가격을 줌
-        data.productsTotal = total ? total + price : price;
-
-        // 기존 데이터(배열)가 있다면 id만 추가하고, 없다면 배열 새로 만듦
-        data.ids = ids ? [...ids, id] : [id];
-
-        // 위와 마찬가지 방식
-        data.selectedIds = selectedIds ? [...selectedIds, id] : [id];
-      });
+      await insertDb(product);
 
       window.location.href = '/order';
     } catch (err) {
       console.log(err);
 
+      //insertDb가 에러가 되는 경우는 이미 제품이 장바구니에 있던 경우임
+      //따라서 다시 추가 안 하고 바로 order 페이지로 이동함
       window.location.href = '/order';
     }
+  });
+}
+
+async function insertDb(product) {
+  // 객체 destructuring
+  const { _id: id, price } = product;
+
+  // 장바구니 추가 시, indexedDB에 제품 데이터 및
+  // 주문수량 (기본값 1)을 저장함.
+  await addToDb('cart', { ...product, quantity: 1 }, id);
+
+  // 장바구니 요약(=전체 총합)을 업데이트함.
+  await putToDb('order', 'summary', (data) => {
+    // 기존 데이터를 가져옴
+    const count = data.productsCount;
+    const total = data.productsTotal;
+    const ids = data.ids;
+    const selectedIds = data.selectedIds;
+
+    // 기존 데이터가 있다면 1을 추가하고, 없다면 초기값 1을 줌
+    data.productsCount = count ? count + 1 : 1;
+
+    // 기존 데이터가 있다면 가격만큼 추가하고, 없다면 초기값으로 해당 가격을 줌
+    data.productsTotal = total ? total + price : price;
+
+    // 기존 데이터(배열)가 있다면 id만 추가하고, 없다면 배열 새로 만듦
+    data.ids = ids ? [...ids, id] : [id];
+
+    // 위와 마찬가지 방식
+    data.selectedIds = selectedIds ? [...selectedIds, id] : [id];
   });
 }
