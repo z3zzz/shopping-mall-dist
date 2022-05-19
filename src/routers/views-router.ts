6ea6a82d9c426e14779bfd8300e8c4bfc0c31b31
kwang <ktkim@elicer.com>
2022-05-19 13:20:13 +0900

    refactor: change url pathname and dir names

--- src/routers/views-router.ts
+++ src/routers/views-router.ts
@@ -6,12 +6,12 @@ const viewsRouter = express.Router();
 // 페이지별로 html, css, js 파일들 라우팅
 viewsRouter.use('/', serveStatic('home'));
 viewsRouter.use('/order/complete', serveStatic('order-complete'));
-viewsRouter.use('/register/user', serveStatic('register-user'));
+viewsRouter.use('/register', serveStatic('register'));
 viewsRouter.use('/login', serveStatic('login'));
-viewsRouter.use('/register/category', serveStatic('register-category'));
-viewsRouter.use('/register/product', serveStatic('register-product'));
+viewsRouter.use('/category/add', serveStatic('category-add'));
+viewsRouter.use('/product/add', serveStatic('product-add'));
 viewsRouter.use('/product/list', serveStatic('product-list'));
-viewsRouter.use('/product', serveStatic('product'));
+viewsRouter.use('/product/detail', serveStatic('product-detail'));
 viewsRouter.use('/cart', serveStatic('cart'));
 viewsRouter.use('/order', serveStatic('order'));
 viewsRouter.use('/order/complete', serveStatic('order-complete'));
@@ -20,8 +20,8 @@ viewsRouter.use('/page-not-found', serveStatic('page-not-found'));
 // views 폴더의 rabbit.png, indexed-db.js 등 라우팅
 viewsRouter.use('/', serveStatic(''));
 
-// views폴더 내의 ${fileName} 폴더 내의 모든 파일을 웹에 띄우며,
-// 이 때 ${fileName}.html 을 기본 파일로 설정함.
+// views폴더 내의 ${resource} 폴더 내의 모든 파일을 웹에 띄우며,
+// 이 때 ${resource}.html 을 기본 파일로 설정함.
 function serveStatic(resource: string) {
   const resourcePath = path.join(__dirname, `../views/${resource}`);
   const option = { index: `${resource}.html` };
