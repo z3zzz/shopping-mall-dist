389fbd91f6247e7c8848473f9539b4c03a92e732
z3zzz <zabra7946@naver.com>
2022-05-22 23:32:09 +0900

    prepare production

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+  entry: './src/views/account/account.js',
+  mode: 'production',
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'bundle.account.js',
+  },
+  plugins: [new MiniCssExtractPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(svg|gif|png|eot|woff|ttf)$/,
+        use: ['url-loader'],
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+      }),
+      new OptimizeCssAssetsPlugin(),
+    ],
+  },
+};
