c7dbccef2af205886e464a7fa38b52b7c105a50f
kwang <ktkim@elicer.com>
2022-05-26 14:53:27 +0900

    add production code

--- dist/src/db/models/user-model.js
+++ dist/src/db/models/user-model.js
@@ -1,82 +1,57 @@
-'use strict';
-var __awaiter =
-  (this && this.__awaiter) ||
-  function (thisArg, _arguments, P, generator) {
-    function adopt(value) {
-      return value instanceof P
-        ? value
-        : new P(function (resolve) {
-            resolve(value);
-          });
-    }
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
-      function fulfilled(value) {
-        try {
-          step(generator.next(value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function rejected(value) {
-        try {
-          step(generator['throw'](value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function step(result) {
-        result.done
-          ? resolve(result.value)
-          : adopt(result.value).then(fulfilled, rejected);
-      }
-      step((generator = generator.apply(thisArg, _arguments || [])).next());
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-  };
-Object.defineProperty(exports, '__esModule', { value: true });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.userModel = exports.UserModel = void 0;
-const mongoose_1 = require('mongoose');
-const user_schema_1 = require('../schemas/user-schema');
+const mongoose_1 = require("mongoose");
+const user_schema_1 = require("../schemas/user-schema");
 const User = (0, mongoose_1.model)('users', user_schema_1.UserSchema);
 class UserModel {
-  findByEmail(email) {
-    return __awaiter(this, void 0, void 0, function* () {
-      const user = yield User.findOne({ email });
-      return user;
-    });
-  }
-  findById(userId) {
-    return __awaiter(this, void 0, void 0, function* () {
-      const user = yield User.findOne({ _id: userId });
-      return user;
-    });
-  }
-  create(userInfo) {
-    console.log({ userInfo });
-    return __awaiter(this, void 0, void 0, function* () {
-      const createdNewUser = yield User.create(userInfo);
-      return createdNewUser;
-    });
-  }
-  findAll() {
-    return __awaiter(this, void 0, void 0, function* () {
-      const users = yield User.find({});
-      return users;
-    });
-  }
-  update({ userId, update }) {
-    return __awaiter(this, void 0, void 0, function* () {
-      const filter = { _id: userId };
-      const option = { returnOriginal: false };
-      const updatedUser = yield User.findOneAndUpdate(filter, update, option);
-      return updatedUser;
-    });
-  }
-  deleteById(userId) {
-    return __awaiter(this, void 0, void 0, function* () {
-      const result = yield User.deleteOne({ _id: userId });
-      return result;
-    });
-  }
+    findByEmail(email) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const user = yield User.findOne({ email });
+            return user;
+        });
+    }
+    findById(userId) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const user = yield User.findOne({ _id: userId });
+            return user;
+        });
+    }
+    create(userInfo) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const createdNewUser = yield User.create(userInfo);
+            return createdNewUser;
+        });
+    }
+    findAll() {
+        return __awaiter(this, void 0, void 0, function* () {
+            const users = yield User.find({});
+            return users;
+        });
+    }
+    update({ userId, update }) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const filter = { _id: userId };
+            const option = { returnOriginal: false };
+            const updatedUser = yield User.findOneAndUpdate(filter, update, option);
+            return updatedUser;
+        });
+    }
+    deleteById(userId) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const result = yield User.deleteOne({ _id: userId });
+            return result;
+        });
+    }
 }
 exports.UserModel = UserModel;
 const userModel = new UserModel();
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,55 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const entryObject = {
+  account: './src/views/account/account.js',
+  'account-orders': './src/views/account-orders/account-orders.js',
+  'account-security': './src/views/account-security/account-security.js',
+  'account-signout': './src/views/account-signout/account-signout.js',
+  admin: './src/views/admin/admin.js',
+  'admin-orders': './src/views/admin-orders/admin-orders.js',
+  'admin-users': './src/views/admin-users/admin-users.js',
+  cart: './src/views/cart/cart.js',
+  'category-add': './src/views/category-add/category-add.js',
+  home: './src/views/home/home.js',
+  login: './src/views/login/login.js',
+  order: './src/views/order/order.js',
+  'order-complete': './src/views/order-complete/order-complete.js',
+  //page_not_found: '',
+  'product-add': './src/views/product-add/product-add.js',
+  'product-detail': './src/views/product-detail/product-detail.js',
+  'product-list': './src/views/product-list/product-list.js',
+  register: './src/views/register/register.js',
+};
+
+const htmls = [];
+for (const key of Object.keys(entryObject)) {
+  htmls.push(
+    new HtmlWebpackPlugin({
+      template: entryObject[key].replace('js', 'html'),
+      inject: false,
+      minify: false,
+      chunks: [key],
+      filename: entryObject[key].replace('js', 'html'),
+    })
+  );
+}
 
 module.exports = {
-  entry: './src/views/account/account.js',
+  entry: entryObject,
   mode: 'production',
   output: {
     path: `${__dirname}/dist`,
-    filename: 'bundle.account.js',
+    filename: 'src/views/[name]/[name].js',
   },
-  plugins: [new MiniCssExtractPlugin()],
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'src/views/[name]/[name].css',
+    }),
+    ...htmls,
+  ],
   module: {
     rules: [
       {
